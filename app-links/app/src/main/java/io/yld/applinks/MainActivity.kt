77a65dbb22497ae37550487bdc80fe343556380c
Messias Junior <messias.junior@yld.io>
2022-07-07 09:24:25 +0100

    create app links poc

---
+++ app-links/app/src/main/java/io/yld/applinks/MainActivity.kt
@@ -0,0 +1,43 @@
+package io.yld.applinks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import io.yld.applinks.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val parameterAdapter by lazy {
+        ParameterAdapter()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding.parametersList)  {
+            adapter = parameterAdapter
+            layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
+            addItemDecoration(DividerItemDecoration(context, LinearLayoutManager.VERTICAL))
+            itemAnimator = DefaultItemAnimator()
+        }
+
+        intent.data?.queryParameterNames?.let(::createParameterViews)
+    }
+
+    private fun createParameterViews(keys: Set<String>) {
+        val models = keys.map { key ->
+            val data = intent.data ?: throw IllegalArgumentException("Intent data cannot be null")
+            val value = data.getQueryParameter(key)
+            ParameterModel(key, value)
+        }
+
+        if (models.isNotEmpty()) {
+            parameterAdapter.submitList(models)
+        }
+    }
+}
